e5d9386194deff38165c527986f8738e28f17eac
Martin Aziz <maziz17j@gmail.com>
2023-08-15 23:17:24 +0300

    Make polybar config better support dual-monitors (fix bspwmrc to support), and add commented support for moving nodes to nodes in sxhkdrc

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -7,9 +7,11 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar
-polybar bottom &
-
-if [[ $(xrandr -q | grep "HDMI-A-1 connected") ]];
+# polybar bottom -c ~/.config/polybar/config-single &
+if [[ $(xrandr -q | grep "HDMI-A-1 connected") ]]; # Second monitor detected - launch both bars with dual monitor config
 then
-	polybar top &
+	polybar top -c ~/.config/polybar/config-dual &
+	polybar bottom -c ~/.config/polybar/config-dual &
+else # Second monitor not detected - launch only bottom bar with single monitor config
+	polybar bottom -c ~/.config/polybar/config-single &
 fi

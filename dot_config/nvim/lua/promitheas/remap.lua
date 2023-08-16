b1dcb68f515dbdbee818b58c427d3fa0c1bd7738
Martin Aziz <maziz17j@gmail.com>
2023-08-16 17:21:11 +0300

    Add keybind to clear search highlighting and move keybinds to correct file

--- dot_config/nvim/lua/promitheas/remap.lua
+++ dot_config/nvim/lua/promitheas/remap.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>c", "<cmd>let @/ = \"\"<CR>") -- clear search highlighting
+
+-- NVim window (splits) keybinds
+vim.keymap.set('n', '<leader>hs', '<cmd>sp<CR>') -- create horizontal split (of current buffer)
+vim.keymap.set('n', '<leader>vs', '<cmd>vs<CR>') -- create vertical split (of current buffer)
+vim.keymap.set('n', '<leader>n', '<cmd>new<CR>') -- create horizontal split (new file)
+vim.keymap.set('n', '<leader>q', '<cmd>q<CR>') -- quit current window
+vim.keymap.set('n', '<leader>j', '<cmd>wincmd j<CR>') -- move cursor to bottom window
+vim.keymap.set('n', '<leader>k', '<cmd>wincmd k<CR>') -- move cursor to top window
+vim.keymap.set('n', '<leader>h', '<cmd>wincmd h<CR>') -- move cursor to left window
+vim.keymap.set('n', '<leader>l', '<cmd>wincmd l<CR>') -- move cursor to right window
+-- TODO: Add keybinds for moving windows around: https://neovim.io/doc/user/windows.html#window-moving
+-- TODO: Lookup keybinds for accepting autocomplete suggestions

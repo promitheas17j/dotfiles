362058682f454a2990e378e4d8560e0fcec11798
Martin Aziz <maziz17j@gmail.com>
2023-10-16 03:29:43 +0300

    Fix rofi keybind prompts for sxhkd bindings and rofi bindings

--- dot_bin/executable_keybind_cheatsheet.sh
+++ dot_bin/executable_keybind_cheatsheet.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -width 1000
+cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -p "keys" -i -no-show-icons -width 1000

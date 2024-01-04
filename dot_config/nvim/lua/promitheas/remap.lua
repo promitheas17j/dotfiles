107681c93a2656992d51e30d7642da11661f33e4
Martin Aziz <maziz17j@gmail.com>
2024-01-04 05:28:43 +0200

    Modofy occupied workspace colour in polybar modules and add fix note to nvim remap.lua

--- dot_config/nvim/lua/promitheas/remap.lua
+++ dot_config/nvim/lua/promitheas/remap.lua
@@ -18,6 +18,7 @@ vim.keymap.set('n', '<leader>H', '<cmd>wincmd H<CR>') -- move current window to 
 vim.keymap.set('n', '<leader>L', '<cmd>wincmd L<CR>') -- move current window to right position
 
 -- CTRL-Backspace to delete previous word
+-- FIX: ctrl+backspace not deleting entire word in nvim
 vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', {noremap = true})
 
 -- TODO: Create mapping to lookup vim keybindings in general, not just telescope ones (might just do this with rofi tbh)

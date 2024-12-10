e4f2443fff753dd1bb006a2152020fbc32f6dbaf
Martin Aziz <maziz17j@gmail.com>
2024-12-11 01:23:40 +0200

    Modify polybar launch script to take into account number of connected monitors more dynamically and not rely on the names of the actual monitors that are connected

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -9,11 +9,15 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 # Make sure that all monitors have been detected by OS before checking how many bars to launch
 sleep 3
 
+connected_monitors=$(xrandr --listmonitors | grep -c ' connected')
+
 # Launch bar
-if [[ $(xrandr -q | grep "HDMI-A-1 connected") ]]; # Second monitor detected - launch both bars with dual monitor config
+if [[ "$connected_monitors" -gt 1 ]]; # Second monitor detected - launch both bars with dual monitor config
 then
+	# Multi-monitor setup
 	polybar top -c ~/.config/polybar/config-dual &
 	polybar bottom -c ~/.config/polybar/config-dual &
-else # Second monitor not detected - launch only bottom bar with single monitor config
+else
+	# Single monitor setup
 	polybar bottom -c ~/.config/polybar/config-single &
 fi

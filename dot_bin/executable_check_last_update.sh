27333f0ac48328540d34b2bf6f647818627820ee
Martin Aziz <maziz17j@gmail.com>
2025-04-29 20:45:21 +0300

    Add kernel mismatch check to last update script

--- dot_bin/executable_check_last_update.sh
+++ dot_bin/executable_check_last_update.sh
@@ -10,3 +10,14 @@ days_since=$(( (now_sec - last_upgrade_sec) / 86400 ))
 if (( days_since > 3 )); then
 	echo "Days since last update: $days_since day(s)"
 fi
+
+# Kernel version check
+running_kernel=$(uname -r)
+installed_kernel=$(pacman -Q linux | awk '{print $2}' | cut -d '-' -f1)
+
+running_kernel_base=$(echo "$running_kernel" | cut -d '-' -f1)
+
+if [[ "$running_kernel_base" != "$installed_kernel" ]];
+then
+	echo "Kernel mismatch detected. Reboot recommended."
+fi

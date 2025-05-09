33de5806c17d7597262a200f78bfc898e39abe5b
Martin Aziz <maziz17j@gmail.com>
2025-05-09 10:52:57 +0300

    Fix script which check last update and kernel mismatch

--- dot_bin/executable_check_last_update.sh
+++ dot_bin/executable_check_last_update.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-last_upgrade=$(grep -m 1 'full system upgrade' /var/log/pacman.log | cut -d ' ' -f 1 | tr -d '[]')
+last_upgrade_date=$(grep -m 1 'full system upgrade' /var/log/pacman.log | cut -d ' ' -f 1 | tr -d '[]')
+last_upgrade_sec=$(date --date="$last_upgrade_date" +%s) # Convert to UNIX timestamp
 
 last_upgrade_sec=$(date -d "$last_upgrade_date" +%s)
 now_sec=$(date +%s)
@@ -8,7 +9,7 @@ now_sec=$(date +%s)
 days_since=$(( (now_sec - last_upgrade_sec) / 86400 ))
 
 if (( days_since > 3 )); then
-	echo "Days since last update: $days_since day(s)"
+	echo "Days since last update: $days_since day(s)" >> /dev/tty
 fi
 
 # Kernel version check
@@ -19,5 +20,5 @@ running_kernel_base=$(echo "$running_kernel" | cut -d '-' -f1)
 
 if [[ "$running_kernel_base" != "$installed_kernel" ]];
 then
-	echo "Kernel mismatch detected. Reboot recommended."
+	echo "Kernel mismatch detected. Reboot recommended." >> /dev/tty
 fi
